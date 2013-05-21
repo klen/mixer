c5d341ec7b381d34cbdcd36b036468e0af8dbb37
Kirill Klenov <horneds@gmail.com>
2013-05-21 15:31:20 +0800

    Fix README, fix setup.

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
     description=read('DESCRIPTION'),
     long_description=read('README.rst'),
     platforms=('Any'),
+    keywords = "django flask sqlalchemy testing mock stub",
 
     author='Kirill Klenov',
     author_email='horneds@gmail.com',
@@ -55,8 +56,12 @@ setup(
         'Natural Language :: English',
         'Natural Language :: Russian',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Utilities',
     ],
 
     packages=['mixer', 'mixer.backend'],
