a06d7d919b416571c01bcb28624b2d7b62804e1e
Kirill Klenov <horneds@gmail.com>
2013-09-11 14:09:50 +0700

    Update version.

--- mixer/__init__.py
+++ mixer/__init__.py
@@ -1,20 +1,19 @@
-"""
-    Mixer is a python module that adds object generation
-    tool for testing your application.
+""" Mixer is a module that adds object generation tool for your application.
 
-    It's supported Django_ ORM, SQLAlchemy_ ORM, Mongoengine_ ODM and etc.
+It's supported Django_ ORM, SQLAlchemy_ ORM, Mongoengine_ ODM and etc.
 
-    Mixer is very useful for testing and fixtures replacement.
+Mixer is very useful for testing and fixtures replacement.
 
-    :copyright: 2013 by Kirill Klenov.
-    :license: BSD, see LICENSE for more details.
+:copyright: 2013 by Kirill Klenov.
+:license: BSD, see LICENSE for more details.
+
 """
 from __future__ import unicode_literals
 
 # Module information
 # ==================
 
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 __project__ = 'mixer'
 __author__ = "horneds <horneds@gmail.com>"
 __license__ = "BSD"
