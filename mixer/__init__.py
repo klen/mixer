c82a17dd717f938b0745898652b9c8a51388f5b3
Kirill Klenov <horneds@gmail.com>
2014-02-10 11:32:40 +0700

    Update version

--- mixer/__init__.py
+++ mixer/__init__.py
@@ -13,7 +13,7 @@ from __future__ import unicode_literals
 # Module information
 # ==================
 
-__version__ = '2.2.0'
+__version__ = '2.3.0'
 __project__ = 'mixer'
 __author__ = "horneds <horneds@gmail.com>"
 __license__ = "BSD"
