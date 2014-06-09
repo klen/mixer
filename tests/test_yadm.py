5e63a2411fdb4ff3ae1ed753ecf3e8d596291379
Kirill Klenov <horneds@gmail.com>
2014-06-09 20:12:16 +0400

    Add beta YADM support

---
+++ tests/test_yadm.py
@@ -0,0 +1,37 @@
+from __future__ import absolute_import
+
+import sys
+
+import pytest
+
+
+pytestmark = pytest.mark.skipif(
+    sys.version_info < (3, 0), reason="requires python3")
+
+
+def test_base():
+    from yadm import Document, fields
+
+    class Author(Document):
+
+        name = fields.StringField
+
+    class BlogPost(Document):
+
+        title = fields.StringField
+        body = fields.StringField
+        tags = fields.ListField(fields.StringField)
+
+        author = fields.ReferenceField(Author)
+
+    from mixer.backend.yadm import mixer
+    assert mixer
+
+    post = mixer.blend(BlogPost, tags=mixer.RANDOM, author__name="Tomas")
+    assert post.title
+    assert post.body
+    assert post.tags
+    assert post.author.name == 'Tomas'
+
+
+# pylama:ignore=D,F0401
