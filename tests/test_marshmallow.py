8ac5fadb1574a00792f26e451a51d2074341c37d
Kirill Klenov <horneds@gmail.com>
2016-11-17 18:28:16 +0300

    Update marshmallow integration.

--- tests/test_marshmallow.py
+++ tests/test_marshmallow.py
@@ -4,27 +4,27 @@ import pytest
 
 class Person(ma.Schema):
 
-    name = ma.fields.String(required=True)
+    name = ma.fields.String()
     status = ma.fields.String(
         required=True,
         validate=ma.validate.OneOf(choices=('user', 'moderator', 'admin')))
-    created = ma.fields.DateTime(required=True)
-    birthday = ma.fields.Date(required=True)
-    is_relative = ma.fields.Bool(required=True)
+    created = ma.fields.DateTime()
+    birthday = ma.fields.Date()
+    is_relative = ma.fields.Bool()
 
 
 class Pet(ma.Schema):
 
-    name = ma.fields.String(required=True)
+    name = ma.fields.String()
     animal_type = ma.fields.String(default='cat')
-    owner = ma.fields.Nested(Person, required=True, many=True)
-    awards = ma.fields.List(ma.fields.Str, required=True)
+    owner = ma.fields.Nested(Person, many=True)
+    awards = ma.fields.List(ma.fields.Str)
 
 
 @pytest.fixture
 def mixer():
-    from mixer.backend.marshmallow import mixer
-    return mixer
+    from mixer.backend.marshmallow import Mixer
+    return Mixer(required=True)
 
 
 def test_mixer(mixer):
