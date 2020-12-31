2a704850a75c836e3197a2ab773b55478d0aa959
Kirill Klenov <horneds@gmail.com>
2020-12-31 16:30:52 +0300

    Bump version: 7.0.2 → 7.0.3

--- mixer/__init__.py
+++ mixer/__init__.py
@@ -14,7 +14,7 @@ from __future__ import unicode_literals
 # Module information
 # ==================
 
-__version__ = "7.0.2"
+__version__ = "7.0.3"
 __project__ = "mixer"
 __author__ = "horneds <horneds@gmail.com>"
 __license__ = "BSD"
